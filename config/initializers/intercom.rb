069f901521cf948c83039bc8f64fea37d7bb3ae5
刘德政 <liudezheng@liudezhengdeMacBook-Pro.local>
2017-06-12 10:18:18 +0800

    Adjust intercom

--- config/initializers/intercom.rb
+++ config/initializers/intercom.rb
@@ -1,7 +1,7 @@
 IntercomRails.config do |config|
   # == Intercom app_id
   #
-  config.app_id = ENV["INTERCOM_APP_ID"] || "d996dxfz"
+  config.app_id = ENV["INTERCOM_APP_ID"] || "awov2i8v"
 
   # == Intercom session_duration
   #
